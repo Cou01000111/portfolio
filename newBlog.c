6e079d4b3bcd8e1531267a23b2848c5f1a80e687
courange <courange.c@gmail.com>
2020-11-09 11:03:12 +0900

    change

--- newBlog.c
+++ newBlog.c
@@ -2,60 +2,53 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-char my_pop_back(char *str)
-{
-    const int len = strlen(str);
-    if( len == 0 ) {
-        return '\0';      // str が空文字列の場合は、'\0' を返す
-    } else {
-        char c = str[len-1];     // 末尾文字を c に代入
-        str[len-1] = '\0';       // 終端記号のヌル文字を次の位置に書き込む
-        return c;                      // 末尾文字を返す
-    }
-}
-
 int main(int argc, char *argv[]){
+  //コマンドライン引数が二つ以上ついていた場合はusageを表示して終了
   if(argc != 2){
     printf("Usage: newb <filename>\n");
     exit(1);
   }
+  //引数がファイル名の上限の255文字を超えていた場合はエラーを表示して終了
   if(strlen(argv[1]) >= 256){
     printf("The maximum number of characters in a file name is 255.");
     exit(1);
   }
-  //ファイルを開いてHTMLファイル作成
   FILE *fp;
+  //新規作成するファイル名
   char fileName[256];
   strcpy(fileName,argv[1]);
+  //新規作成するhtmlファイルの中身
   char html[2048];
-  /*
-  printf("whats new logs name?:");
-  fgets(fileName, 256, stdin);
-  my_pop_back(fileName);
-  */
+  //テンプレートファイルを読み込み
   fp = fopen("./blog/blogTmp.html", "r");
+  //読み込みミスった時の処理
   if(fp == NULL){
     printf("htmlTmpファイルが読み込めませんでした");
     return -1;
   }
+  //バッファのb
   char b[256];
+  //blogTmp.htmlからhtmlテンプレートを一行ずつNULLが読み込まれるまで読み込み
   while((fgets(b,256,fp)) != NULL){
+    //パスが含まれているところのみ書き換え
     if(strstr(b, "axios.get('/portfolio/blog/')")){
-      printf("%s\n",fileName);
       sprintf(b,"      axios.get('/portfolio/blog/%s.md')\n",fileName);
     }
     strcat(html,b);
   }
   fclose(fp);
   //printf("%s",html);
+  //新規作成するhtmlファイルのPath
   char newFilePathHtml[256];
+  //新規作成するMdファイルのPath
   char newFilePathMd[256];
-
+  //Path生成
   sprintf(newFilePathHtml, "./blog/%s.html",fileName );
   sprintf(newFilePathMd, "./blog/%s.md",fileName );
 
+  //readonlyでファイルを開きファイルを新規作成
   fp = fopen(newFilePathHtml, "r");
+  //すでに同じ名前のファイルがある場合上書き保存するか聞く
   if(fp != NULL){
     printf("Can I overwrite the file?[y/n]\n");
     char ans;
@@ -63,10 +56,12 @@ int main(int argc, char *argv[]){
     if(ans == 'n')return -1;
   }
 
+  //htmlファイルをもう一度開く
   fp = fopen(newFilePathHtml, "w");
+  //htmlに書き込み
   fputs(html, fp);
   fclose(fp);
-
+  //mdファイル新規作成
   fp = fopen(newFilePathMd, "w");
   fclose(fp);
   //ファイルを新しく
