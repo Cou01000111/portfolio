d1f1bfc306ed0dcdfd5ce970fb918d65bd8f60f7
courange <courange.c@gmail.com>
2020-11-19 15:17:34 +0900

    change

--- headerCreate/Program.cs
+++ headerCreate/Program.cs
@@ -9,28 +9,54 @@ namespace headerCreate
   {
     static void Main(string[] args)
     {
+      List<string> headerTmp = new List<string>();
+      string[] tmpFiles = Directory.GetFiles(@"../","tmp.html",SearchOption.AllDirectories);
+      string tmpFile = tmpFiles[0];
+      StreamReader tmp = new StreamReader(tmpFile, Encoding.GetEncoding("UTF-8"));
       bool inHeader = false;
-      foreach (var file in Directory.GetFiles(@"../","*.html",SearchOption.AllDirectories))
-      {
-        StreamReader sr = new StreamReader(file, Encoding.GetEncoding("Shift_JIS"));
-        string s = sr.ReadToEnd();
-        sr.Close();
-
-        if(s == @"<header>"){
-          Console.WriteLine("In header.");
-          if(inHeader)continue;
-          else if(s == @"</header>"){
-            Console.WriteLine("Out header");
-          }
+      string line;
+      while((line = tmp.ReadLine()) != null){
+        if(line.Contains(@"<header>") || line.Contains(@"</header>") || inHeader){
+          headerTmp.Add(line);
+          if(line.Contains(@"</header>"))inHeader = false;
+          else inHeader = true;
+          continue;
         }
+      }
+      foreach(var tmpLine in headerTmp){
+        Console.WriteLine(tmpLine);
+      }
 
+      inHeader = false;
+      foreach (var file in Directory.GetFiles(@"../","html.html",SearchOption.AllDirectories))
+      {
+        Console.WriteLine("loading html...");
+        StreamReader sr = new StreamReader(file, Encoding.GetEncoding("UTF-8"));
+
+        line = null;
+        List<string> texts = new List<string>();
+        while((line = sr.ReadLine()) != null){
+          if(line.Contains(@"<header>") || line.Contains(@"</header>") || inHeader){
+            if(line.Contains(@"</header>")){
+              inHeader = false;
+              texts.AddRange(headerTmp);
+              }
+            else inHeader = true;
+            continue;
+          }
+          texts.Add(line);
+        }
+        sr.Close();
         StreamWriter sw = new StreamWriter(
           file,
           false,
-          System.Text.Encoding.GetEncoding("Shift_JIS")
+          System.Text.Encoding.GetEncoding("UTF-8")
           );
-
-        sw.Write(s);
+        foreach (var text in texts)
+        {
+          sw.Write(text);
+          sw.Write("\n");
+        }
         sw.Close();
       }
     }
