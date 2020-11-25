3d3cffcbce96cfc86c52a5798e00b1697a65ad94
courange <courange.c@gmail.com>
2020-11-26 00:58:30 +0900

    change

--- blog/pass.js
+++ blog/pass.js
@@ -1,6 +1,20 @@
 'use strict';
+function reaTextFile(file){
+  var rawFile = new XMLHttpRequest();
+  var fileName = "./pass.txt"
+  var allText;
+  rawFile.open("GET", fileName, false);
+  rawFile.onreadystatechange = () => {
+    if(rawFile.readyState === 4){
+      if(rawFile.status === 200 || rawFile.status == 0){
+        allText = rawFile.responseText;
+      }
+    }
+  }
+}
+
 var pass = document.getElementById("password").value;
-if(pass == 'courange0723'){
+if(pass == allText){
   axios.get('/portfolio/blog/daigaku.md')
     .then(response => {
       document.getElementById('markdownArea').innerHTML = marked(response.data)
