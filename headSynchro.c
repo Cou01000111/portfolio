4444db452b0dbd19d61a1f37a9c3949c6d4f5f01
courange <courange.c@gmail.com>
2020-11-25 09:29:20 +0900

    add system

---
+++ headSynchro.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+  system(".\\headCopy\\bin\\Release\\netcoreapp3.1\\headCopy.exe");
+  return 0;
+}
